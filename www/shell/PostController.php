f175e0b8da2fa3895bb2ad803087125bf4dcb3c3
limitium <limitium@gmail.com>
2011-11-08 17:04:52 +0300

    post sorted by date

--- www/shell/PostController.php
+++ www/shell/PostController.php
@@ -5,10 +5,20 @@ class PostController {
     public function getList(Request $request) {
         $pageDir = getPath('pages/');
         $posts = array();
+        $sortedByDate = array();
         foreach (scandir($pageDir) as $fileId) {
             if (is_file($pageDir . $fileId)) {
-                $posts[] = Post::load($fileId);
+                $sortedByDate[] = array('fname' => $fileId, 'time' => filemtime($pageDir . $fileId));
             }
+        }
+
+        function sorter($a, $b) {
+            return $a['time'] < $b['time'] ? 1 : -1;
+        }
+
+        usort($sortedByDate, "sorter");
+        foreach ($sortedByDate as $post) {
+            $posts[] = Post::load($post['fname']);
         }
         $this->posts = $posts;
     }
